f334d3de2303b543fa31d4fc88823f98c9881116
Emanuel Donalds <emanueldonalds@edu.novia.fi>
2019-04-04 20:25:28 +0300

    Added identity

--- Novia.TypeManagement/Presentation/Presentation.Web/Startup.cs
+++ Novia.TypeManagement/Presentation/Presentation.Web/Startup.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Novia.TypeManagement.Configuration;
+using Novia.TypeManagement.Presentation.Web.Data;
+using Novia.TypeManagement.Presentation.Web.Models;
 
 namespace Novia.TypeManagement.Presentation.Web
 {
+    using Novia.TypeManagement.Presentation.Web.Services;
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -24,18 +24,19 @@ namespace Novia.TypeManagement.Presentation.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            {
-                services.Configure<CookiePolicyOptions>(options =>
-                {
-                    // This lambda determines whether user consent
-                    // for non-essential cookies is needed for a given request.
-                    options.CheckConsentNeeded = context => true;
-                    options.MinimumSameSitePolicy = SameSiteMode.None;
-                });
-                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-                ServiceContainerConfigurator.ConfigureServices(
-                Configuration.GetConnectionString("DefaultConnection"), services);
-            }
+            services.AddDbContext<TypeIdentityDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+                services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<TypeIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+
+            ServiceContainerConfigurator.ConfigureServices(
+            Configuration.GetConnectionString("DefaultConnection"), services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,7 @@ namespace Novia.TypeManagement.Presentation.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -51,7 +53,8 @@ namespace Novia.TypeManagement.Presentation.Web
             }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            // app.UseCookiePolicy();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
