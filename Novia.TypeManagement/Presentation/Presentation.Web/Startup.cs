da60be8091ceb5e8c0f86f41199ff824946aba24
Emanuel Donalds <emanueldonalds@edu.novia.fi>
2019-04-04 18:25:59 +0300

    Working add

--- Novia.TypeManagement/Presentation/Presentation.Web/Startup.cs
+++ Novia.TypeManagement/Presentation/Presentation.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Novia.TypeManagement.Configuration;
 
 namespace Novia.TypeManagement.Presentation.Web
 {
@@ -23,15 +24,18 @@ namespace Novia.TypeManagement.Presentation.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                services.Configure<CookiePolicyOptions>(options =>
+                {
+                    // This lambda determines whether user consent
+                    // for non-essential cookies is needed for a given request.
+                    options.CheckConsentNeeded = context => true;
+                    options.MinimumSameSitePolicy = SameSiteMode.None;
+                });
+                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                ServiceContainerConfigurator.ConfigureServices(
+                Configuration.GetConnectionString("DefaultConnection"), services);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
